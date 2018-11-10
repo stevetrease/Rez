d19ffb5f4c41e1e073c33d71a8574ac7a798a888
Steve <stevetrease@gmail.com>
2018-11-10 17:18:43 +0000

    Initial Commit

---
+++ Rez/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rez
+//
+//  Created by Steve on 10/11/2018.
+//  Copyright © 2018 Steve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
