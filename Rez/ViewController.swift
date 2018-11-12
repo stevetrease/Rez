5b8b9ffe5746ab1844e70fd4af0fd857923a90a2
Steve <stevetrease@gmail.com>
2018-11-12 11:06:52 +0000

    First sort of working version.

--- Rez/ViewController.swift
+++ Rez/ViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var resLabel: UILabel!
-    @IBOutlet weak var sCountLabel: UILabel!
     @IBOutlet weak var orientationLabel: UILabel!
+    @IBOutlet weak var screenTwoLabel: UILabel!
 
 
     override func viewDidLoad() {
@@ -21,39 +21,79 @@ class ViewController: UIViewController {
         print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
         // Do any additional setup after loading the view, typically from a nib.
         
+        screenTwoLabel.isHidden = true
         drawScreen()
         
         let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver (self, selector: #selector(ViewController.drawScreen), name: UIScreen.didConnectNotification, object: nil)
-        notificationCenter.addObserver (self, selector: #selector(ViewController.drawScreen), name: UIScreen.didDisconnectNotification, object: nil)
+        notificationCenter.addObserver (self, selector: #selector(ViewController.screenDidConnect), name: UIScreen.didConnectNotification, object: nil)
+        notificationCenter.addObserver (self, selector: #selector(ViewController.screenDidDisconnect), name: UIScreen.didDisconnectNotification, object: nil)
         notificationCenter.addObserver (self, selector: #selector(ViewController.drawScreen), name: UIScreen.modeDidChangeNotification, object: nil)
+        
     }
     
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
+        drawScreen()
+    }
+    
+    
+    @objc func screenDidConnect () {
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
+        let extScreen = UIScreen.screens.last
+        print (extScreen?.bounds.width)
+        print (extScreen?.bounds.height)
+        print (extScreen?.currentMode!.size.height)
+        print (extScreen?.currentMode!.size.height)
+        drawScreen()
+    }
+    
+    @objc func screenDidDisconnect () {
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
         drawScreen()
     }
     
     
     @objc func drawScreen () {
-        let screenSize = UIScreen.main.bounds
-        let screenWidth = Int(screenSize.width)
-        let screenHeight = Int(screenSize.height)
-        let screenScale = Int(UIScreen.main.scale)
-        let screenCount = UIScreen.screens.count
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
         
-        let orientationString: String
+        let screen = UIScreen.main
+        
+        var screenWidth = Int(screen.bounds.width)
+        var screenHeight = Int(screen.bounds.height)
+        let screenScale = Int(screen.scale)
+        resLabel.text = "\(screenWidth) x \(screenHeight) (\(screenScale)x)"
+        screenWidth = Int(screen.currentMode!.size.width)
+        screenHeight = Int(screen.currentMode!.size.height)
+        resLabel.text = resLabel.text! + " = " + "\(screenWidth) x \(screenHeight)"
+        
         if UIDevice.current.orientation.isLandscape {
-            orientationString = "Landscape"
+            orientationLabel.text = "Landscape"
         } else {
-            orientationString = "Portrait"
+            orientationLabel.text = "Portrait"
         }
         
-        resLabel.text = "\(screenWidth) x \(screenHeight) (\(screenScale))"
-        sCountLabel.text = "\(screenCount)"
-        orientationLabel.text = orientationString
+
+        if UIScreen.screens.count == 1 {
+            screenTwoLabel.isHidden = true
+        } else {
+            screenTwoLabel.isHidden = false
+            
+            screenTwoLabel.text = ""
+            print ("screens: \(UIScreen.screens.count)")
+            for screen in UIScreen.screens {
+                var sWidth = Int(screen.bounds.width)
+                var sHeight = Int(screen.bounds.height)
+                screenTwoLabel.text = screenTwoLabel.text! + "\n" + "\(sWidth) x \(sHeight) = "
+                print ("- \(sWidth) x \(sHeight)")
+                
+                sWidth = Int(screen.currentMode!.size.width)
+                sHeight = Int(screen.currentMode!.size.height)
+                screenTwoLabel.text = screenTwoLabel.text! + "\(sWidth) x \(sHeight)" + "\n"
+            }
+        }
+        
     }
-
+    
 }
-
